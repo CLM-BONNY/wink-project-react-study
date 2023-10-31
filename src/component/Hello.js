50aa652b8d3f3843ecd19dde0de42f2d0d989cb5
김민선 <alstjs4739@kookmin.ac.kr>
2023-10-31 22:12:08 +0900

    feat. 2주차-2(event 처리)

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,14 +1,40 @@
-import World from "./World";
-// import styles from "./Hello.module.css";
-import * as style from "./styles";
-
 export default function Hello() {
+  function showName() {
+    console.log("mike");
+  }
+
+  function showAge(age) {
+    console.log(age);
+  }
+
+  // function showText(e) {
+  //   console.log(e.target.value);
+  // }
+
+  function showText(txt) {
+    console.log(txt);
+  }
+
   return (
     <div>
       <h1>Hello</h1>
-      <World />
-      {/* <div className={styles.box}>Hello</div> */}
-      <style.Box>Hello</style.Box>
+      <button onClick={showName}>Show name</button>
+      <button
+        onClick={() => {
+          showAge(30);
+        }}
+      >
+        Show age
+      </button>
+      {/* <input type="text" onChange={showText} /> */}
+      {/* <input type="text" onChange={(e) => console.log(e.target.value)} /> */}
+      <input
+        type="text"
+        onChange={(e) => {
+          const txt = e.target.value;
+          showText(txt);
+        }}
+      />
     </div>
   );
 }
